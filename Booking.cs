17d6789d06ee267c7b4cf47d16cf42a3cb87714d
riham <riham91.alsiyabi@gmail.com>
2025-07-27 14:55:59 +0400

    Date and time of the appointment

--- Booking.cs
+++ Booking.cs
@@ -10,7 +10,7 @@ namespace CodeLineHealthCareCenter
     {
         public int Id { get; set; } // Unique identifier for the booking
         public int PatientId { get; set; } // Identifier for the patient making the booking
-
-        public int ClinicId { get; set; }   // Identifier for the clinic where the booking is made  
+        public int ClinicId { get; set; }   // Identifier for the clinic where the booking is made
+        public DateTime AppointmentDate { get; set; } // Date and time of the appointment
     }
 }
