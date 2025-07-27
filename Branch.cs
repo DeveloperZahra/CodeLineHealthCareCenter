5cf5eef12cd6fefb8604377ebc26ea54c3b0451b
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-27 14:53:58 +0400

    address public attribute in class of branch

--- Branch.cs
+++ Branch.cs
@@ -10,6 +10,8 @@ namespace CodeLineHealthCareCenter
     {
         public int Id { get; set; } // ID public attribute for every branch 
         public string Name { get; set; } // Name public attribute for every branch
+        public string Address { get; set; } // address public attribute in class of branch 
+
 
     }
 }
