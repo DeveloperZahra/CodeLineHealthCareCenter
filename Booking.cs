1eac532ad6952cb154ef28166563f010a57a5439
riham <riham91.alsiyabi@gmail.com>
2025-07-27 14:54:19 +0400

    Identifier for the patient making the booking

--- Booking.cs
+++ Booking.cs
@@ -9,5 +9,6 @@ namespace CodeLineHealthCareCenter
     class Booking
     {
         public int Id { get; set; } // Unique identifier for the booking
+        public int PatientId { get; set; } // Identifier for the patient making the booking
     }
 }
