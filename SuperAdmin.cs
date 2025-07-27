2be654e5300b8d657dccea6125588c56c6785aa3
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-27 15:18:03 +0400

    Class representing a SuperAdmin user who has the highest level of access

---
+++ SuperAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLineHealthCareCenter
+{
+    // Class representing a SuperAdmin user who has the highest level of access
+    public class SuperAdmin
+    {
+    }
+}
