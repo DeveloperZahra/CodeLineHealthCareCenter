33b2a8bf555472c9925e6ef3c08538a09483cbf9
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-27 14:54:15 +0400

    IsActive public attribute in class of branch

--- Branch.cs
+++ Branch.cs
@@ -11,6 +11,9 @@ namespace CodeLineHealthCareCenter
         public int Id { get; set; } // ID public attribute for every branch 
         public string Name { get; set; } // Name public attribute for every branch
         public string Address { get; set; } // address public attribute in class of branch 
+        public bool IsActive { get; set; }  // IsActive public attribute in class of branch
+
+        // 
 
 
     }
