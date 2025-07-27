fdf9ce6d96464b648e1e6c8728e9640205775171
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-27 14:21:27 +0400

    Base class representing a general user in the system

---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLineHealthCareCenter
+{
+    // Base class representing a general user in the system
+    public class User
+    {
+    }
+}
