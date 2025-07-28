a42726f8f9ef8c1bd071065fc21a6817a14e8a78
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-28 08:23:50 +0400

    List to store all admins

--- Models/SuperAdmin.cs
+++ Models/SuperAdmin.cs
@@ -15,21 +15,9 @@ namespace CodeLineHealthCareCenter
         // List to store all departments
         private List<Department> Departments = new List<Department>();
 
-        // Method to add a new department to a specific branch
-        public void AddDepartment(Department department, int branchId)
-        {
-            Branch targetBranch = Branches.Find(b => b.Id == branchId);
-            if (targetBranch != null && department != null)
-            {
-                department.BranchId = branchId;
-                Departments.Add(department);
-                Console.WriteLine($"Department '{department.Name}' added to Branch ID: {branchId}.");
-            }
-            else
-            {
-                Console.WriteLine("Branch not found or invalid department.");
-            }
-        }
+
+        // List to store all admins
+        private List<Admin> Admins = new List<Admin>();
 
         // Method to assign a new admin to the system
         public void AddAdmin(Admin admin)
