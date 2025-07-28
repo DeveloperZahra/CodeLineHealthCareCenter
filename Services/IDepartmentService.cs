e15c19db44c4bf43ff9b65e34bc39c1a1fd023dc
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-28 14:20:36 +0400

    Displays all departments

--- Services/IDepartmentService.cs
+++ Services/IDepartmentService.cs
@@ -7,7 +7,8 @@ namespace HospitalSystemTeamTask.Services
     public interface IDepartmentService
     {
         void CreateDepartment(int branchId); // Creates a new department for a specific branch.
+        void GetAllDepartments(); // Displays all departments
+      
 
-       
     }
 }
