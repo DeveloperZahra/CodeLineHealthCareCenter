684196d3b6a2fd4ac4af5470568d5cac40b612c1
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-28 08:42:04 +0400

    The Admin class inherits from the base User class

---
+++ Models/ِAdmincs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLineHealthCareCenter.Models
+{
+    // The Admin class inherits from the base User class.
+    // Represents a user with administrative privileges over a specific department and branch.
+    public class _ِAdmincs
+    {
+    }
+}
