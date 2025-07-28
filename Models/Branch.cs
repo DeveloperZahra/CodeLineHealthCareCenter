6d4b5f233e3b918e3795fda7431e8f95e71ccdcd
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-28 09:21:59 +0400

    add aditional attributes to branch class

--- Models/Branch.cs
+++ Models/Branch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,25 @@ namespace CodeLineHealthCareCenter
 {
     class Branch
     {
-        public int Id { get; set; } // ID public attribute for every branch 
-        public string Name { get; set; } // Name public attribute for every branch
-        public string Address { get; set; } // address public attribute in class of branch 
-        public bool IsActive { get; set; }  // IsActive public attribute in class of branch
+        // 1. Class Field...
+        public int BranchId;// ID public attribute for every branch 
+        public string BranchName;// Name public attribute for every branch
+        public string BranchAddress; // address public attribute in class of branch 
+        public bool BranchStatus; // current status of Branch; true represent is open, false represent close 
+        public DateOnly BranchEstablished; // Establish Date OF Branch
+        public static int BranchCount =0;
+        public List<Floor> Floors = new List<Floor>();
+        
+        // 2. class property... 
 
-        // 
+
+        // 3. class method...
+
+        // 4. Class Constructor...
+
+
+
+
 
 
     }
