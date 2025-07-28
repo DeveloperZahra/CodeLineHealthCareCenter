4e091ce6aa33cc306df8d5b1a55d61a0419e0dbb
riham <riham91.alsiyabi@gmail.com>
2025-07-28 13:31:38 +0400

    public int branchId; // ID of the branch the clinic is located in

--- Models/Clinic.cs
+++ Models/Clinic.cs
@@ -11,6 +11,7 @@ namespace CodeLineHealthCareCenter
         // 1. Class Fields
         public int id; // Unique identifier for the clinic
         public string name; // Name of the clinic
-        public int departmentId; // ID of the department the clinic belongs to
+        public int departmentId; // ID of the department the clinic 
+        public int branchId; // ID of the branch the clinic is located in
     }
 }
