5fd493fa61cc1ef94544ca6c1bcb7cb039bb3671
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-28 10:31:05 +0400

    prepare property for every field

--- Models/Branch.cs
+++ Models/Branch.cs
@@ -7,17 +7,18 @@ using System.Threading.Tasks;
 
 namespace CodeLineHealthCareCenter
 {
+    // Represents a branch of the hospital, including its details and associated floors.
     class Branch
     {
         // 1. Class Field...
-        public int BranchId;// ID public attribute for every branch 
-        public string BranchName;// Name public attribute for every branch
-        public string BranchAddress; // address public attribute in class of branch 
-        public bool BranchStatus; // current status of Branch; true represent is open, false represent close 
-        public DateOnly BranchEstablished; // Establish Date OF Branch
-        public static int BranchCount =0;
-        public List<Floor> Floors = new List<Floor>();
-        
+        private int BranchId;// Unique identifier for the branch. 
+        private string BranchName;// The name of the branch.
+        private string BranchAddress; //The address where the branch is located. 
+        private bool BranchStatus; // Indicates whether the branch is currently active (true = open, false = closed).
+        private DateOnly BranchEstablished; // The date when the branch was established.
+        private static int BranchCount =0; // Keeps track of the total number of branches created.
+        private List<Floor> Floors = new List<Floor>(); //  List of floors that belong to this branch.
+
         // 2. class property... 
 
 
