e8fbe59c4f7225d3149fbf5ce4d717e8fc7b8927
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-28 14:23:00 +0400

    Updates a department's details using branchId and departmentId.

--- Services/IDepartmentService.cs
+++ Services/IDepartmentService.cs
@@ -8,7 +8,12 @@ namespace HospitalSystemTeamTask.Services
     {
         void CreateDepartment(int branchId); // Creates a new department for a specific branch.
         void GetAllDepartments(); // Displays all departments
-      
+        void UpdateDepartment(int branchId, int departmentId); //Updates a department's details using branchId and departmentId.
+        void SetDepartmentActiveStatus(int departmentId, bool isActive);
+        void GetDepartmentByName(string department);
+        void GetDepartmentByid(int did);
+        string GetDepartmentName(int depId);
+
 
     }
 }
