3d713731ab3af78865f4ca6fb2904fd212888521
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-28 16:02:53 +0400

    Displays all departments in a branch by branch name

--- Services/IBranchDepartmentService.cs
+++ Services/IBranchDepartmentService.cs
@@ -12,5 +12,7 @@ namespace HospitalSystemTeamTask.Services
         void GetBranchsByDepartment(int departmentId); // Displays all branches that contain a specific department.
         void UpdateBranchDepartment(int branchId, int departmentId, string newDepartmentName); // Updates the details of a branch-department relationship.
         void GetBranchDep(int departmentId, int branchId); // Displays details of a specific branch-department relationship.
+        void GetDepartmentsByBranchName(string branchName); // Displays all departments in a branch by branch name
+
     }
 }
