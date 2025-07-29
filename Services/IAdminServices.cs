c5e97df9a3a8b1410c6c61eda90b26b39f0b59ab
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-29 12:00:02 +0400

    create interface for admin services

---
+++ Services/IAdminServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLineHealthCareCenter.Services
+{
+    public interface IAdminServices
+    {
+        void AddAdmin(string name, string email, string password, string nationalId, string phoneNumber, int branchId, int departmentId);
+    }
+}
