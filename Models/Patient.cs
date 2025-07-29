4ddb1a13ad989668d5367e1734969f0c99d18300
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-29 21:41:37 +0400

    Adds a new patient to the system.

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalSystemTeamTask.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CodeLineHealthCareCenter
 {
-    class Patient : User
+    class Patient : User, IPatientService
     {
         // 1. ======================== Class Fields ==========================
         public DateTime DateOfBirth { get; set; } // Patient's date of birth
@@ -30,8 +31,28 @@ namespace CodeLineHealthCareCenter
             IsActive = true;
         }
 
-       //================================================ Patients Methods ===================================
+        //4. ================================================ Patients Methods ===================================
+        /// implement method in IPatientServices Interface
+        // 4.1 Adds a new patient to the system.
+        public void AddPatient(string name, DateTime dateOfBirth, string email, string password, string nationalId, string phoneNumber, string gender, string city)
+        {
+            // Check if patient with the same email already exists
+            bool exists = patients.Exists(p => p.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+            if (exists)
+            {
+                Console.WriteLine("Patient with this email already exists!");
+                return;
+            }
 
+            // Create a new Patient object
+            Patient newPatient = new Patient(name, dateOfBirth, email, password, nationalId, phoneNumber, gender, city);
+
+            // Add to the static list
+            patients.Add(newPatient);
+
+            // Confirmation message
+            Console.WriteLine($"Patient '{newPatient.UserName}' added successfully with ID: {newPatient.UserId}");
+        }
 
 
 
