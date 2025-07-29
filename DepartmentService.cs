7b508278a2cd8e9e278a50fa33b2bead9458f965
riham <riham91.alsiyabi@gmail.com>
2025-07-29 14:48:36 +0400

    Check if there are no departments

--- DepartmentService.cs
+++ DepartmentService.cs
@@ -11,12 +11,17 @@ namespace CodeLineHealthCareCenter
         // 1. Get all departments
         public void GetAllDepartments()
         {
-            if (items.Count == 0)
+            if (items.Count == 0) // Check if there are no departments
             {
                 Console.WriteLine(" No departments found.");
                 return;
             }
 
+            Console.WriteLine(" List of Departments:"); 
+            foreach (var dept in items) 
+            {
+                Console.WriteLine($"ID: {dept.DepartmentId}, Name: {dept.DepartmentName}"); 
+            }
         }
     }
 }
