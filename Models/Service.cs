6287d8e35fe376fd7faea9eb9d0ef7be2cc12247
riham <riham91.alsiyabi@gmail.com>
2025-07-29 11:54:38 +0400

    Add a new service to the list

--- Models/Service.cs
+++ Models/Service.cs
@@ -10,5 +10,16 @@ namespace CodeLineHealthCareCenter
     class Service
     {
         protected List<Service> services = new List<Service>(); // List to store services
+
+        // Add a new service to the list
+        public virtual void Add(ServiceModel service)
+        {
+            services.Add(service);
+            Console.WriteLine(" Service added successfully.");
+        }
+
+
+
+
     }
 }
