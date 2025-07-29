a1051bdc1990908be6e9bd4ee46d52d59ad2a3df
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-29 22:07:17 +0400

    Add "using CodeLineHealthCareCenter.Models; "

--- Services/ISuperAdminSevices.cs
+++ Services/ISuperAdminSevices.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HospitalSystemTeamTask.Services;
+using CodeLineHealthCareCenter.Models;
 
 
 namespace CodeLineHealthCareCenter.Services
