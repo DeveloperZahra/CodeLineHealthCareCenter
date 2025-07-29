d7ff1588f1b2edb49d56755caeb7c5493a60a9ea
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-29 22:06:53 +0400

    add "using HospitalSystemTeamTask.Services; + using CodeLineHealthCareCenter.Models;"

--- Services/IAdminServices.cs
+++ Services/IAdminServices.cs
@@ -4,7 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using HospitalSystemTeamTask.Services;
+using CodeLineHealthCareCenter.Models;
+
+
+
 namespace CodeLineHealthCareCenter.Services
+
 {
     public interface IAdminServices
     {
