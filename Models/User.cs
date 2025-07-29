423d39a381c2f92be8918c42007e976ed4833457
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-29 19:50:20 +0400

    Prints all users with a specific Doctor role.

--- Models/User.cs
+++ Models/User.cs
@@ -562,7 +562,11 @@ namespace CodeLineHealthCareCenter
                     foreach (var user in roleUsers2)
                         DisplayUser(user);
                     break;
+                // Prints all users with a specific Doctor role.
                 case "Doctor":
+                    var roleUsers3 = Doctor.doctors.Where(u => u.Role.Equals(roleName, StringComparison.OrdinalIgnoreCase));
+                    foreach (var user in roleUsers3)
+                        DisplayUser(user);
                     break;
                 case "Patient":
                     break;
