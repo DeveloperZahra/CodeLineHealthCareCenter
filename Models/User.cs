771dc988198096f1b3150287a599479674e52bc5
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-29 12:31:05 +0400

    add more attribute in user class which is Gender attribute

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalSystemTeamTask.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +7,44 @@ using System.Threading.Tasks;
 
 namespace CodeLineHealthCareCenter
 {
-    // Base class representing a general user in the system
-    public class User
+    // Base class for all users in the system (patients, doctors, admins, super admins).
+    public class User : IUserService
     {
-        // Unique identifier for the user
-        public int Id { get; set; }
+        // 1. ============== private feild ==============
+        private string password;
 
-        // User's full name
-        public string FullName { get; set; }
+        //2.  ============== Properties =================
+        public static int UserCount = 0;
+        public string UserId { get; set; } // User Id 
+        public string UserName { get; set; } // name of the user
+        public string Email { get; set; } // User's email address
+        public string NationalID { get; set; } // National ID of the user
+        public string PhoneNumber { get; set; } // Phone number of the user
+        public string Gender { get; set; } // Gender for all user 
+        public string Role { get; protected set; } // Role (Doctor, Patient, Admin, SuperAdmin)
+        public bool IsActive { get; set; } // Account status
 
-        // User's email address (used for login or contact)
-        public string Email { get; set; }
 
-
-        // Property to store the user's password (should be handled securely in real applications)
-        public string Password { get; set; }
-
-        // Virtual method to get the role of the user (can be overridden in derived classes)
-        public virtual string GetRole()
+        // 3. ====================== Encapsulated Password ====================
+        public string Password
         {
-            // Default role for a basic user
-            return "User";
+            get => password;
+            set
+            {
+                if (value.Length >= 3)
+                    password = value;
+                else
+                    throw new ArgumentException("Password must be at least 6 characters long.");
+            }
+        }
+
+        // 4. ======================= Methods of IUserServices Interface ==========================
+        /// Implement Methods which all users will 
+        // 4.1 Checks if the email and password match any active user
+        public bool AuthenticateUser(string email, string password, string Role)
+        {
+            
+           
         }
 
 
