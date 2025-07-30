f18abbe96db762fe76ab86a16e3168e7aca9f29a
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-30 14:54:08 +0400

    create function for Registers a new Patien (Sign Up).

---
+++ Models/AuthServices.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLineHealthCareCenter.Models
+{
+    public class AuthServices
+    {
+        // Registers a new Patien (Sign Up).
+        public static void SignUp()
+        {
+            Console.WriteLine("\n=== SIGN UP ===");
+
+            // 1️⃣ Collect user input
+            string name = UserData.EnterUserName();
+            string email = UserData.EnterUserEmail();
+            string nationalId = UserData.EnterNationalID();
+            string phone = UserData.EnterPhoneNumber();
+            string gender = UserData.EnterGender();
+            bool isActive = UserData.EnterIsActive();
+            string hashedPassword = UserData.EnterPasswordForSignUp();
+            DateTime dateOfBirth = UserData.EnterDateOfBirth();
+
+            // 2️⃣ Validate if email already exists
+            if (Patient.patients.Any(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase)))
+            {
+                Console.WriteLine("❌ Email already registered. Please sign in.");
+                return;
+            }
+
+            // 3️. create a new Patient object
+            Patient newUser = new Patient(name, dateOfBirth, email, hashedPassword ,nationalId, phone, gender)
+            {
+                UserName = name,
+                DateOfBirth = dateOfBirth,
+                Email = email,
+                NationalID = nationalId,
+                PhoneNumber = phone,
+                Gender = gender,
+                IsActive = isActive,
+                Password = hashedPassword
+            };
+
+            // 4️⃣ Add user to the registered list
+            Patient.patients.Add(newUser);
+            Console.WriteLine($"✅ User '{name}' registered successfully!");
+        }
+
+        
+    }
+}
