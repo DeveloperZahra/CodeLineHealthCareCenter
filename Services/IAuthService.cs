7ed53dbb175e7d019ccc172d176b6c020a642079
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-30 11:19:09 +0400

    update in functions in the IAuthServices interface to be include just Login function

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -5,8 +5,6 @@ namespace HospitalSystemTeamTask.Services
 {
     public interface IAuthService
     {
-        JwtTokenResponse GenerateToken(User user);
-        Task SaveTokenToCookie(string token);
-        Task<int> GetUserIdFromToken();
+        void Login();
     }
 }
