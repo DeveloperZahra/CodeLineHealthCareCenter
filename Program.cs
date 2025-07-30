131c75b3597d196e18f5659ed2881c9cec4d2709
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-07-30 11:46:31 +0400

    Set the email of the new user

--- Program.cs
+++ Program.cs
@@ -103,7 +103,7 @@ namespace CodeLineHealthCareCenter
                     {
                         Id = users.Count + 1,  // Assign a unique Id based on the current number of users + 1
                         FullName = name, // Set the full name of the new user
-                        Email = email,
+                        Email = email,  // Set the email of the new user
                         Password = password,
                         BranchId = branchId,
                         DepartmentId = adminDept
