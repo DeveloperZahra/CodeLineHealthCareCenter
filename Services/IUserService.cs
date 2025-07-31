3a232d93e496ba5ee503d052b49a1c1b166e119f
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-31 14:43:25 +0400

    put UserId as int

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -8,11 +8,11 @@ namespace HospitalSystemTeamTask.Services
     public interface IUserService
     {   
         bool AuthenticateUser(string email, string password, string Role);
-        void DeactivateUser(string UserId, string role);
+        void DeactivateUser(int UserId, string role);
         bool EmailExists(string email, string role);
-        void GetUserById(string UserId, string Role);
-        void UpdatePassword(string UserId,string Role, string currentPassword, string newPassword);
-        void UpdateUser(string UserId, string Role);
+        void GetUserById(int UserId, string Role);
+        void UpdatePassword(int UserId,string Role, string currentPassword, string newPassword);
+        void UpdateUser(int UserId, string Role);
         void GetUsersByRole(string roleName);
     }
 }
