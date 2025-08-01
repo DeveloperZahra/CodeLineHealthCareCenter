0f051d873f42bb8569196497bfa4a771187ae5aa
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-01 19:20:39 +0400

    declare Removes an admin by their ID function

--- Services/IAdminServices.cs
+++ Services/IAdminServices.cs
@@ -15,5 +15,12 @@ namespace CodeLineHealthCareCenter.Services
     public interface IAdminServices
     {
         void AddAdmin(string name, string email, string password, string nationalId, string phoneNumber,string gender, int branchId, int departmentId);
+        void RemoveAdmin(int adminId); // Removes an admin by their ID
+        void UpdateAdmin(int adminId); // Updates an admin's details by their ID
+        void ViewAdmin(int adminId); // Views details of a specific admin by their ID
+        void ViewAllAdmins(); // Views all admins in the system
+        void ViewAdminsByBranch(int branchId); //   Views all admins in a specific branch
+        void ViewAdminsByDepartment(int departmentId); //   Views all admins in a specific department
+
     }
 }
