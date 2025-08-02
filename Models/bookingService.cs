180edde19a0054f83c9c5c41445726a9e54d125f
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-08-02 10:07:35 +0400

    Internal class responsible for handling booking-related operations

---
+++ Models/bookingService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CodeLineHealthCareCenter.Models
+{
+    internal class bookingService
+    {
+        internal static void UpdateBookingStatus(int bookingId, string newStatus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
