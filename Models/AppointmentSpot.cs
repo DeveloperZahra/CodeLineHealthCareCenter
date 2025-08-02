048d5d8713f87d440b7c6a947c16ab6f899c786a
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-02 13:15:36 +0400

    create AppointmentSpot class

---
+++ Models/AppointmentSpot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLineHealthCareCenter.Models
+{
+    class AppointmentSpot
+    {
+        public DateTime SpotDateTime { get; set; }
+        public bool IsBooked { get; set; }
+        public int? PatientId { get; set; } // Track which patient booked it
+    }
+}
