8ef5640182cfeb1330dc4d40c05f6ff4352d4314
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-02 04:15:15 +0400

    make PauseForUser() as public

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ namespace CodeLineHealthCareCenter
         }
 
         // Waits for user input before continuing
-        static void PauseForUser()
+        public static void PauseForUser()
         {
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
